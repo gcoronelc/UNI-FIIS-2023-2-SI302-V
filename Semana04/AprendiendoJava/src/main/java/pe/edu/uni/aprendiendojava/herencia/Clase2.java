185daae8b8f855aefbe5ba27653e086beab00b3b
Gustavo Coronel <gcoronelc@gmail.com>
2023-09-18 09:31:40 -0500

    Semana 4

---
+++ Semana04/AprendiendoJava/src/main/java/pe/edu/uni/aprendiendojava/herencia/Clase2.java
@@ -0,0 +1,9 @@
+package pe.edu.uni.aprendiendojava.herencia;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class Clase2 extends Clase1{
+
+}
---
+++ Semana04/AprendiendoJava/src/main/java/pe/edu/uni/aprendiendojava/herencia/Clase4.java
@@ -0,0 +1,9 @@
+package pe.edu.uni.aprendiendojava.herencia;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class Clase4 extends Clase2{
+
+}
